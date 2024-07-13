121414a1d825adf0d28e01b50d73232d746ca6d0
Chain-Wu Lee <chainwu_lee@yahoo.com>
2024-07-13 11:20:27 +0800

    working (code) but not working (solution)

--- mod.py
+++ mod.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sympy as sp
 from sympy import Symbol, Eq, Number
+import torch
 
 import modulus
 from modulus.sym.hydra import instantiate_arch, ModulusConfig
@@ -9,13 +10,84 @@ from modulus.sym.domain import Domain
 from modulus.sym.geometry.primitives_1d import Point1D
 from modulus.sym.domain.constraint import (
     PointwiseBoundaryConstraint,
-    PointwiseBoundaryConstraint,
 )
+from modulus.sym.domain.monitor.pointwise import PointwiseMonitor
 from modulus.sym.domain.validator import PointwiseValidator
+from modulus.sym.domain.inferencer.pointwise import PointwiseInferencer
 from modulus.sym.key import Key
 from modulus.sym.node import Node
-
+from modulus.sym.utils.io.plotter import ValidatorPlotter, InferencerPlotter
+import matplotlib.pyplot as plt
+import sys
+import os
 from cstr import CSTR, k_0, EoverR
+
+class ReactorPlotter(InferencerPlotter):
+    def __call__(self, invar, outvar):
+        try:
+            # get input variables
+            t, T_c = invar["t"][:,0], invar["T_c"][:,0]
+            #print("shape", t.shape, T_c.shape)
+            C_A, T = outvar["C_A"], outvar["T"]
+            #print(C_A, T)
+        
+            # make plot
+            plt.figure(figsize=(20,5), dpi=100)
+            plt.suptitle("CSTR Reactor")
+            fig, ax=plt.subplots(2,1)
+            ax[0].plot(t, C_A)
+            ax[0].set_xlim([0, 10])
+            #ax[0].set_ylim([0, 1])
+            ax[0].set_title("Reactant Concentration (mol/L)")
+        
+            ax[1].plot(t, T)
+            ax[1].set_title("Reactor Temp (K)")
+            ax[1].set_xlim([0, 10])
+            #ax[1].set_ylim([300, 450])
+            plt.tight_layout()
+
+            return [(fig, "custom_plot")]
+        except:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
+
+
+#def add_inferencer(nodes, domain):
+    # add inferencer data
+#    tdict=np.linspace(0, 10, 1000).reshape(-1,1)
+#    tcdict=np.full((1000), 290.).reshape(-1,1)
+#    inf290 = PointwiseInferencer(
+#        nodes=nodes,
+#        invar={'t': tdict, 'T_c': tcdict},
+#        output_names=["C_A", "T"],
+#        batch_size=1000,
+#        requires_grad=False,
+#        plotter=InferencerPlotter(),
+#    )
+#    domain.add_inferencer(inf290, "Reactor-290")
+
+#    tcdict=np.full((1000), 300.).reshape(-1,1)
+#    inf300 = PointwiseInferencer(
+#        nodes=nodes,
+#        invar={'t': tdict, 'T_c': tcdict},
+#        output_names=["C_A", "T"],
+#        requires_grad=False,
+#        batch_size=1000,
+#        #plotter=ReactorPlotter(),
+#    )
+#    domain.add_inferencer(inf300, "Reactor-300")
+        
+#    tcdict=np.full((1000), 305.).reshape(-1,1)
+#    inf305 = PointwiseInferencer(
+#        nodes=nodes,
+#        invar={'t': tdict, 'T_c': tcdict},
+#        output_names=["C_A", "T"],
+#        batch_size=1000,
+#        requires_grad=False,
+#        plotter=InferencerPlotter(),
+#    )
+#    domain.add_inferencer(inf305, "Reactor-305")
 
 @modulus.sym.main(config_path="conf", config_name="config")
 def run(cfg: ModulusConfig) -> None:
@@ -43,11 +115,13 @@ def run(cfg: ModulusConfig) -> None:
     IC = PointwiseBoundaryConstraint(
         nodes=nodes,
         geometry=geo,
-        outvar={"T": 350, "C_A": 0.5}, 
+        outvar={"T": 350, "C_A": 0.5, "T__t":0, "C_A__t":0}, 
         batch_size=cfg.batch_size.IC,
         lambda_weighting={
             "T": 1.0,
             "C_A": 1.0,
+            "T__t":1.0, 
+            "C_A__t":0,
         },
         parameterization={t: 0, "T_c":300},
     )
@@ -62,6 +136,34 @@ def run(cfg: ModulusConfig) -> None:
         parameterization=param_range,
     )
     domain.add_constraint(interior, "interior")
+
+    # add monitors
+    tnpy=np.linspace(0, 10, 1000).reshape(-1,1)
+    tcnpy=np.full((1000), 300.).reshape(-1,1)
+    gmon = PointwiseMonitor(
+        invar={'t':tnpy, 'T_c':tcnpy},
+        output_names=["T", "C_A"],
+        metrics={
+            "Temp": lambda var: torch.mean(var["T"]),
+            "Temp_Std": lambda var: torch.std(var["T"]),
+            "Conc": lambda var: torch.mean(var["C_A"]),
+            "Conc_Std": lambda var: torch.std(var["C_A"]),
+        },
+        nodes=nodes,
+    )
+    domain.add_monitor(gmon)
+
+    tdict=np.linspace(0, 10, 1000).reshape(-1,1)
+    tcdict=np.full((1000), 300.).reshape(-1,1)
+    inf300 = PointwiseInferencer(
+        nodes=nodes,
+        invar={'t': tdict, 'T_c': tcdict},
+        output_names=["C_A", "T"],
+        requires_grad=False,
+        batch_size=1000,
+        plotter=ReactorPlotter()
+    )
+    domain.add_inferencer(inf300, "Reactor-300")
 
 # make solver
     slv = Solver(cfg, domain)
